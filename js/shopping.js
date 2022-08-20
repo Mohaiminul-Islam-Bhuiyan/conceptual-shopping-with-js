c16b6efcde3f02dc7fdb65e763f0ff73e8c02eed
Mohaiminul-Islam-Bhuiyan <rafe.aust@gmail.com>
2022-08-20 12:47:08 +0600

    full

---
+++ js/shopping.js
@@ -0,0 +1,61 @@
+
+const cartArray = [];
+
+function display(cartProduct){
+    let totalPrice = 0;
+    const tableBody = document.getElementById('cart-products');
+    tableBody.innerHTML = '';
+    for (let i = 0; i < cartProduct.length; i++)
+    {
+        // console.log(cartArray[i].productName, cartArray[i].productPrice);
+        const name = cartArray[i].productName;
+        const price = cartArray[i].productPrice;
+
+        totalPrice = totalPrice + price;
+
+        const tr = document.createElement('tr');
+
+        tr.innerHTML = 
+        `
+            <th>${i+1}</th>
+            <td>${name}</td>
+            <td>${price}</td>
+        `;
+
+        tableBody.appendChild(tr);
+    }
+
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = 
+    `
+        <th></th>
+        <td>Total Price</td>
+        <td>${totalPrice}</td>
+    `;
+    
+    tableBody.appendChild(tr);
+}
+
+function addToCart(element){
+    // console.log(element.parentNode.parentNode.children)
+    // console.log(element.parentNode.parentNode.children[0].innerText)
+    // console.log(element.parentNode.parentNode.children[1].children[0].innerText)
+
+    const productName = element.parentNode.parentNode.children[0].innerText;
+
+    const productPrice = element.parentNode.parentNode.children[1].children[0].innerText;
+
+    const productObject = {
+        productName: productName,
+        productPrice: parseFloat(productPrice)
+    }
+
+    cartArray.push(productObject);
+    // console.log(cartArray)
+    // console.log(cartArray.length)
+
+    document.getElementById('total-added-product').innerText = cartArray.length;
+    
+    display(cartArray);
+}
